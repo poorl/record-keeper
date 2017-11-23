459c79d31e29678bc57c0416e8a7ad5e76931174
kitkitng <kitkitng@LAPTOP-4AHQT30G>
2017-11-23 19:29:46 +0800

    Implement API delegate

---
+++ src/main/java/com/butlerx/api/RecordApi.java
@@ -0,0 +1,62 @@
+package com.butlerx.api;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.butlerx.admin.RecordApiDelegate;
+import com.butlerx.admin.model.RecordDetails;
+import com.butlerx.admin.model.RecordRequest;
+import com.butlerx.admin.model.ResponseMessage;
+
+public class RecordApi implements RecordApiDelegate {
+
+	@Override
+	public ResponseEntity<RecordDetails> confirmRecord(Long id) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.confirmRecord(id);
+	}
+
+	@Override
+	public ResponseEntity<RecordDetails> createRecord(RecordRequest body) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.createRecord(body);
+	}
+
+	@Override
+	public ResponseEntity<ResponseMessage> deleteRecord(Long id) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.deleteRecord(id);
+	}
+
+	@Override
+	public ResponseEntity<Void> finishLoc(String id) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.finishLoc(id);
+	}
+
+	@Override
+	public ResponseEntity<RecordDetails> getRecord(Long id) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.getRecord(id);
+	}
+
+	@Override
+	public ResponseEntity<List<RecordDetails>> listRecord() {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.listRecord();
+	}
+
+	@Override
+	public ResponseEntity<Void> startLoc(String id) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.startLoc(id);
+	}
+
+	@Override
+	public ResponseEntity<RecordDetails> updateRecord(Long id, RecordRequest body) {
+		// TODO Auto-generated method stub
+		return RecordApiDelegate.super.updateRecord(id, body);
+	}
+
+}
